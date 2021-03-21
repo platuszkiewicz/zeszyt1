08a55eaca707ad9fb515f4598b4ff541ad76e090
Piotr Latuszkiewicz <piotr.latuszkiewicz@gmail.com>
2021-03-21 20:27:42 +0100

    dodanie widoku cyklu

--- Zeszyt1.www/scripts/script.js
+++ Zeszyt1.www/scripts/script.js
@@ -50,6 +50,11 @@ function InitializePage(callback) {
             LoadEvents();
         });
     });
+    $('#cycleBtn').on('click', function () {
+        $("#pageContent").load("./sub/cycle.html", null, function () {
+            LoadCycle();
+        });
+    });
 
 
     //załadowanie modala
@@ -77,6 +82,89 @@ function InitializePage(callback) {
 
 
 
+}
+
+// https://www.wolframalpha.com/input/?i=%28x-A%29%5E2%2B%28y-B%29%5E2%3DR%5E2%2C+y%3Dax%2Bb
+
+function LoadCycle() {
+    // Dynamicznie zmieniane:
+    // 1) Kropka na osi pokazująca który jest dzien cyklu
+    // 2) Informacja ile dni pozostalo do kolejnego cyklu
+    $.getJSON('./data/dates.json', function (dataString) {
+        var dates = [];
+        for (var i = 0; i < dataString.length; i++) {
+            var dateString = dataString[i].Date.split("-");
+            date = new Date(Number(dateString[0]), Number(dateString[1]) - 1, Number(dateString[2]));
+            dates.push(date);
+        }
+        var cycleDay = dateDiffInDays(dates[dates.length - 1], new Date()) + 1;
+
+        $.getJSON('./data/stats.json', function (dataString) {
+            setTimeout(200, function () { });
+            var median = dataString.Median;
+            var predicitionDate = dates[dates.length - 1];
+            predicitionDate.setDate(predicitionDate.getDate() + Number(median));
+            var daysToNextCycle = dateDiffInDays(new Date(), predicitionDate);
+
+            //console.log("Który dzień cyklu (cycleDay): " + cycleDay);
+            //console.log("Data poczatku kolejnego cyklu (predictionDate): " + predicitionDate);
+            //console.log("Liczba dni do następnego cyklu (daysToNextCycle): " + daysToNextCycle);
+
+            // *** Rysowanie wykresu ***
+
+            var c = document.getElementById("svg-cycle").contentDocument;
+            var todayCircle = c.getElementById("path4510");
+            var todayLabel = c.getElementById("text5116");
+            var daysLeft = c.getElementById("tspan4537");
+
+            c.getElementById("text4539").textContent = String(daysToNextCycle).padStart(2, '0');
+            c.getElementById("text4539").setAttribute("style", "font-size: 50px;");
+
+            c.getElementById("text5116").textContent = String(cycleDay).padStart(2, '0');
+            c.getElementById("text5116").setAttribute("style", "font-size: 40px;");
+
+            if (daysToNextCycle < 0) {
+                c.getElementById("text4539").textContent = String(-daysToNextCycle).padStart(2, '0');
+                c.getElementById("text4539-4").textContent = "Opóźnienie";
+                c.getElementById("text4539-4").setAttribute("style", "font-size: 50px;");
+                c.getElementById("text4539-1").textContent = "dni";
+                c.getElementById("text4539-1").setAttribute("style", "font-size: 50px;");
+            }
+
+            cycleDay = Math.min(cycleDay, 28);
+
+            // Parametry narysowanego SVG:
+            B_cx = 250;
+            B_cy = 250;
+            R = 175;
+
+            var alpha = 2 * Math.PI / 28 * (28 - cycleDay) + 2 * Math.PI / 28 / 2;
+            // r-nie prostej y = a * x + b
+            var a = Math.tan(Math.PI/2 - alpha);
+            var b = B_cy - B_cx * Math.tan(Math.PI/2 - alpha); 
+            // r-nie okręgu (dużego) (x - Sx)^2 + (y - Sy)^2 = R^2 o środku (Sx, Sy)
+            var A = B_cx;
+            var B = B_cy;
+            // wynik przecięcia równań
+            var x1 = (-Math.sqrt(-a * a * A * A + a * a * R * R - 2 * a * A * b + 2 * a * A * B - b * b + 2 * b * B - B * B + R * R) - a * b + a * B + A) / (a * a + 1);
+            var y1 = (-a * Math.sqrt(-a * a * A * A + a * a * R * R - 2 * a * A * b + 2 * a * A * B - b * b + 2 * b * B - B * B + R * R) + a * a * B + a * A + b) / (a * a + 1);
+            var x2 = (Math.sqrt(-a * a * A * A + a * a * R * R - 2 * a * A * b + 2 * a * A * B - b * b + 2 * b * B - B * B + R * R) - a * b + a * B + A) / (a * a + 1);
+            var y2 = (a * Math.sqrt(-a * a * A * A + a * a * R * R - 2 * a * A * b + 2 * a * A * B - b * b + 2 * b * B - B * B + R * R) + a * a * B + a * A + b) / (a * a + 1);
+
+            if (cycleDay > 14) {
+                todayCircle.setAttribute("cx", x1 + "px");
+                todayCircle.setAttribute("cy", y1 + "px");
+                todayLabel.setAttribute("x", x1 - 21 + "px");
+                todayLabel.setAttribute("y", y1 + 21 / 2 + "px");
+            } else {
+                todayCircle.setAttribute("cx", x2 + "px");
+                todayCircle.setAttribute("cy", y2 + "px");
+                todayLabel.setAttribute("x", x2 - 21 + "px");
+                todayLabel.setAttribute("y", y2 + 21/2 + "px");
+            }
+
+        }); // --- koniec getJSON stats
+    }); // ------- koniec getJSON dates
 }
 
 function LoadCalendar(year) {
@@ -463,4 +551,15 @@ function mobileCheck() {
     var check = false;
     (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true })(navigator.userAgent || navigator.vendor || window.opera);
     return check;
+}
+
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+// a and b are javascript Date objects
+function dateDiffInDays(a, b) {
+    // Discard the time and time-zone information.
+    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
 }
